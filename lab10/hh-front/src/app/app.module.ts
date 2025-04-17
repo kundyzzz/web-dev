38920513cf7f9faedd0939560f6d5f5255cd58a8
kundyzzz <159567667+kundyzzz@users.noreply.github.com>
2025-04-18 03:11:33 +0500

    Update app.module.ts

--- lab10/hh-front/src/app/app.module.ts
+++ lab10/hh-front/src/app/app.module.ts
@@ -1,13 +1,7 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { bootstrapApplication } from '@angular/platform-browser';
 
-// Нет необходимости объявлять AppComponent в @NgModule
-
 bootstrapApplication(AppComponent, {
   providers: [
-    // Можно добавить провайдеры, если нужны
   ]
 }).catch(err => console.error(err));
